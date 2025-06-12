132820754a884e9da8be68029509c918cfcc5694
matdadi <drahmat11@gmail.com>
2025-06-12 21:54:57 +0700

    try to insert quranAPI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,103 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<QuranAPI> fetchAyah() async {
+  String url = 'https://api.alquran.cloud/v1/ayah/114:1/id.muntakhab';
+  final response = await http.get(
+    Uri.parse(url),
+  );
+  
+  if (response.statusCode == 200) {
+    print(response.statusCode);
+    return QuranAPI.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    print(response.statusCode);
+    throw Exception('Failed to fetch Ayah.');
+  }
+}
+
+class AyahData {
+  final int number;
+  final String text;
+  final int numberInSurah;
+  final int juz;
+  final int manzil;
+  final int page;
+  final int ruku;
+  final int hizbQuarter;
+  final bool sajda;
+
+  const AyahData({
+    required this.number,
+    required this.text,
+    required this.numberInSurah,
+    required this.juz,
+    required this.manzil,
+    required this.page,
+    required this.ruku,
+    required this.hizbQuarter,
+    required this.sajda
+  });
+
+  factory AyahData.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      'number': int number,
+      'text': String text,
+      'numberInSurah': int numberInSurah,
+      'juz': int juz,
+      'manzil': int manzil,
+      'page': int page,
+      'ruku': int ruku,
+      'hizbQuarter': int hizbQuarter,
+      'sajda': bool sajda
+      } =>
+          AyahData(
+              number: number,
+              text: text,
+              numberInSurah: numberInSurah,
+              juz: juz,
+              manzil: manzil,
+              page: page,
+              ruku: ruku,
+              hizbQuarter: hizbQuarter,
+              sajda: sajda
+          ),
+      _ => throw const FormatException('Failed to load ayah.'),
+    };
+  }
+}
+
+class QuranAPI {
+  final int code;
+  final String status;
+  final AyahData data;
+
+  const QuranAPI(
+  {
+    required this.code,
+    required this.status,
+    required this.data
+  });
+
+  factory QuranAPI.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'code': int code,
+        'status': String status,
+        'data': AyahData data
+      } => QuranAPI(
+          code: code,
+          status: status,
+          data: data
+      ),
+      _ => throw const FormatException('Failed to load QuranAPI.'),
+    };
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -35,6 +133,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _currentFortune = ':)';
+  late Future<QuranAPI> futureAyah;
 
   final _fortuneList = [
     "A life built on integrity",
@@ -56,6 +155,12 @@ class _MyHomePageState extends State<MyHomePage> {
       _currentFortune = _fortuneList[fortune];
     });
   }
+  
+  @override
+  void initState() {
+    super.initState();
+    futureAyah = fetchAyah();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,12 +172,16 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Image.asset("asset/image/fortune_cookie.png", width: 200, height: 200, fit: BoxFit.cover,),
             Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  _currentFortune,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
+              child: FutureBuilder<QuranAPI>(
+                builder: (context, key) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      key.data!.data.text,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  );
+                }, future: null,
               ),
             ),
             ElevatedButton(onPressed: _randomFortune, child: Text('Click for fortune',),)
